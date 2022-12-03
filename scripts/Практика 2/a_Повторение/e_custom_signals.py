d595f6c96f01908cab656b194d45cbf68f5e19a8
vlad <vlad_1995.02@mail.ru>
2022-12-03 11:40:22 +0300

    e_custom_signals done

--- scripts/Практика 2/a_Повторение/e_custom_signals.py
+++ scripts/Практика 2/a_Повторение/e_custom_signals.py
@@ -15,13 +15,76 @@
   (пароль можно показать в терминале в захешированном виде)
 """
 
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore
 
 
 class Window(QtWidgets.QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+
+        self.initChilds()
+        self.initUi()
+        self.initSignals()
+
+    def initUi(self):
+        self.label = QtWidgets.QLabel("Пройдите регистрацию")
+        self.pushButtonRegistration = QtWidgets.QPushButton("Регистрация")
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.pushButtonRegistration)
+
+        self.setLayout(layout)
+
+    def initSignals(self):
+        self.pushButtonRegistration.clicked.connect(self.registrationDialog.exec)
+        self.registrationDialog.received.connect(
+            lambda received: self.label.setText(f"Добро пожаловать {received[0]}, ваш пароль {received[1]}")
+        )
+
+    def initChilds(self):
+        self.registrationDialog = RegistrationDialog()
+
+
+class RegistrationDialog(QtWidgets.QDialog):
+    received = QtCore.Signal(tuple)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.initUi()
+        self.initSignals()
+
+    def initUi(self):
+        self.lineEditLogin = QtWidgets.QLineEdit()
+        self.lineEditLogin.setPlaceholderText("Введите логин")
+
+        self.lineEditPassword = QtWidgets.QLineEdit()
+        self.lineEditPassword.setPlaceholderText("Введите пароль")
+
+        self.pushButton = QtWidgets.QPushButton("Зарегистрироваться")
+
+        layoutLineEdit = QtWidgets.QHBoxLayout()
+        layoutLineEdit.addWidget(self.lineEditLogin)
+        layoutLineEdit.addWidget(self.lineEditPassword)
+
+        layoutMain = QtWidgets.QVBoxLayout()
+        layoutMain.addLayout(layoutLineEdit)
+        layoutMain.addWidget(self.pushButton)
+
+        self.setLayout(layoutMain)
+
+    def initSignals(self):
+        self.pushButton.clicked.connect(self.onPushButtonClicked)
+
+    def onPushButtonClicked(self):
+        login = self.lineEditLogin.text()
+        password = self.lineEditPassword.text()
+
+        self.received.emit((login, password))
+
+        self.close()
 
 
 if __name__ == "__main__":
