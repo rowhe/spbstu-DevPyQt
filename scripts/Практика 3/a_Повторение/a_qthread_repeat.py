734eca0f7705c3ecc610e213f2938b1562b8f82d
vlad <vlad_1995.02@mail.ru>
2022-12-10 11:08:49 +0300

    q_timer done

--- scripts/Практика 3/a_Повторение/a_qthread_repeat.py
+++ scripts/Практика 3/a_Повторение/a_qthread_repeat.py
@@ -6,14 +6,101 @@
 Предлагается создать приложение-которое будет
 с некоторой периодичностью вызывать определённую функцию.
 """
+import time
 
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore, QtGui
 
 
 class Window(QtWidgets.QWidget):
 
     def __init__(self, parent=None):
         super().__init__(parent)
+
+        self.initTimers()
+        self.initUi()
+
+        self.initSignals()
+
+    def initUi(self):
+        """
+
+        :return:
+        """
+
+        self.labelDateTime = QtWidgets.QLabel()
+        self.labelDateTime.setAlignment(QtCore.Qt.AlignmentFlag.AlignRight)
+
+        self.pushButton = QtWidgets.QPushButton("Начать выполнение")
+        self.pushButton.setCheckable(True)
+
+        self.plainTextEdit = QtWidgets.QPlainTextEdit()
+        self.plainTextEdit.setReadOnly(True)
+
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.pushButton)
+        layout.addWidget(self.plainTextEdit)
+        layout.addWidget(self.labelDateTime)
+
+        self.setLayout(layout)
+
+    def initTimers(self):
+        """
+
+        :return:
+        """
+
+        self.timerTime = QtCore.QTimer()
+        self.timerTime.setInterval(1000)
+        self.timerTime.start()
+
+        self.timerHandle = QtCore.QTimer()
+        self.timerHandle.setInterval(2000)
+
+    def initSignals(self):
+        """
+
+        :return:
+        """
+
+        self.timerTime.timeout.connect(self.setDateTime)
+        self.timerHandle.timeout.connect(self.handleFunc)
+
+        self.pushButton.clicked.connect(self.setTimerHandleStatus)
+
+    def setDateTime(self):
+        """
+
+        :return:
+        """
+
+        dateTime = QtCore.QDateTime.currentDateTime()
+        dateStr = dateTime.toString("yyyy-MM-dd HH:mm:ss ddd")
+        self.labelDateTime.setText(dateStr)
+
+    def handleFunc(self):
+        """
+
+        :return:
+        """
+
+        self.plainTextEdit.appendPlainText(time.ctime() + " Вызов функции: " + str(self.handleFunc))
+
+    def setTimerHandleStatus(self, status):
+        """
+
+        :return:
+        """
+
+        if status:
+            self.plainTextEdit.appendPlainText(time.ctime() + " Обработка запущена")
+            self.timerHandle.start()
+        else:
+            self.plainTextEdit.appendPlainText(time.ctime() + " Обработка остановлена")
+            self.timerHandle.stop()
+
+    def closeEvent(self, event: QtGui.QCloseEvent) -> None:
+        self.timerHandle.stop()
+        self.timerTime.stop()
 
 
 if __name__ == "__main__":
